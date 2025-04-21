4b7396016812be66b8cedc04b50f716c03c8e9f0
Alexis Gadbin <alexis@gadbin.com>
2025-04-21 21:57:53 +0200

    Initial Commit

---
+++ CoolUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoolUi
+//
+//  Created by Alexis Gadbin on 21/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoolUi/CoolUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolUiApp.swift
+//  CoolUi
+//
+//  Created by Alexis Gadbin on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoolUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
